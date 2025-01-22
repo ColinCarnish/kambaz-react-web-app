8ce3c98dbc47345ad8b36bfe055780074001c903
ColinCarnish <147413477+ColinCarnish@users.noreply.github.com>
2025-01-22 16:01:33 -0500

    Added name header

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,7 @@ import teslabotImage from "/images/teslabot.jpg";
 export default function Lab1() {
     return (
       <div id="wd-lab1">
+        <h1>Name: Colin Carnish, Section: 202530_1</h1>  
         <h2>Lab 1</h2>
         <h3>HTML Examples</h3>
         <div id="wd-tables">
