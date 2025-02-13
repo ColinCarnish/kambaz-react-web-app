4fa229de82b1bf6c9693eb2208b031694d243f56
ColinCarnish <147413477+ColinCarnish@users.noreply.github.com>
2025-02-12 20:26:08 -0500

    Update index.tsx

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,43 +1,51 @@
+import { FaSearch, FaPlus} from "react-icons/fa";
+import GreenCheckmark from "../Modules/GreenCheckMark";
+
 export default function Assignments() {
-    return (
-      <div id="wd-assignments">
-        <input placeholder="Search for Assignments"
-               id="wd-search-assignment" />
-        <button id="wd-add-assignment-group">+ Group</button>
-        <button id="wd-add-assignment">+ Assignment</button>
-        <h3 id="wd-assignments-title">
-          ASSIGNMENTS 40% of Total <button>+</button> </h3>
-        <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link">
-            A1 - ENV1 + HTML
-          </a>
-          <div className="wd-assignment-details">
-            <span>Multiple Modules | <b>Not available until</b> May 6 at 12:00am</span>
-            <br />
-            <span><b>Due</b> May 13 at 11:59pm | 100 pts</span>
-          </div>
-        </li>
-        <li className="wd-assignment-list-item">
-            <a href="#/Kambaz/Courses/1234/Assignments/124" className="wd-assignment-link">
-            A2 - CSS + BOOTSTRAP
-          </a>
-          <div className="wd-assignment-details">
-            <span>Multiple Modules | <b>Not available until</b> May 13 at 12:00am</span>
-            <br />
-            <span><b>Due</b> May 20 at 11:59pm | 100 pts</span>
-          </div>
-        </li>
-        <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link">
-            A3 - JAVASCRIPT + REACT
-          </a>
-          <div className="wd-assignment-details">
-            <span>Multiple Modules | <b>Not available until</b> May 20 at 12:00am</span>
-            <br />
-            <span><b>Due</b> May 27 at 11:59pm | 100 pts</span>
-          </div>
-        </li>
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <div className="position-relative w-25">
+          <FaSearch className="position-absolute text-muted" style={{ left: "10px", top: "50%", transform: "translateY(-50%)" }} />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="form-control ps-4"
+          />
+        </div>
+        <div>
+          <button className="btn btn-light border me-2">
+            <FaPlus className="me-1" /> Group
+          </button>
+          <button className="btn btn-danger text-white">
+            <FaPlus className="me-1" /> Assignment
+          </button>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between align-items-center border-bottom pb-2 mb-3">
+        <h3 className="mb-0">ASSIGNMENTS</h3>
+        <span className="text-muted">40% of Total</span>
+      </div>
+      <ul className="list-group">
+        {[
+          { id: 1, title: "A1", available: "May 6", due: "May 13" },
+          { id: 2, title: "A2", available: "May 13", due: "May 20" },
+          { id: 3, title: "A3", available: "May 20", due: "May 27" },
+        ].map((assignment) => (
+          <li key={assignment.id} className="list-group-item d-flex justify-content-between align-items-center border-start border-success ps-3">
+            <div>
+              <a href="#/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link text-black text-decoration-none fw-bold fs-6 d-block">
+                {assignment.title}
+              </a>
+              <div className="text-muted small">
+                Multiple Modules | <b>Not available until</b> {assignment.available}
+                <br />
+                <b>Due:</b> {assignment.due} at 11:59pm | 100 pts
+              </div>
+            </div>
+            <GreenCheckmark />
+          </li>
+        ))}
       </ul>
     </div>
   );
