8cb1740f185549ac2547fad1456df6f602ddac1d
ColinCarnish <147413477+ColinCarnish@users.noreply.github.com>
2025-02-19 20:13:29 -0500

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,14 +7,13 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Colin Carnish</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
     </div>
 );}
